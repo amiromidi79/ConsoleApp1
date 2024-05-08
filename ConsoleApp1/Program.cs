389a64fca40ce3e6555ac2fb0604816f8f886681
S07C06 <S07C06@S07C06>
2024-05-08 08:46:43 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿//alireza ghasemi
+//amir hossein omidi
+//reza zare
+static void Sum(int a, int b)
+{
+    Console.WriteLine(a + b);
+}
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Sum(a, b);
+
